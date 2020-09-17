1d8237effa7961f33b1f2618d101dd5e6b89ecfc
Warren Niu <warrenniu@gmail.com>
2020-09-17 16:14:37 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ while row_index < src.length do
   element_index = 0
   lowest_value_element = ""
   while element_index < src[row_index].length do
-    if src[row_index][element_index].min
+    if src[row_index][element_index] < lowest_value_element.to_i
     lowest_value_element = src[row_index][element_index]
   end
     element_index += 1
